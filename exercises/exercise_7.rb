245d50f59c78a000bc32b9fc04b843a48dd4e32d
Dawson Ley <anarchonist@protonmail.com>
2018-12-04 21:51:23 +0000

    exercise 7 completed

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,13 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "What is the name of your store?"
+store_name = gets.chomp
+user_store = Store.create(name: store_name)
+if user_store.errors.any?
+  user_store.errors.each do |attribute, message|
+    puts "#{attribute} #{message}"
+  end
+end
+
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validate :must_carry_mens_or_womens_apparel
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+
+  def must_carry_mens_or_womens_apparel
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:mens_apparel, "ye got no clothes ye bassclawt")
+    end
+  end
 end
