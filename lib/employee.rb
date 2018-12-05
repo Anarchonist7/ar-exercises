fb1dbebfa67a4f6cacccd7757f4d569c9c85d64c
Dawson Ley <anarchonist@protonmail.com>
2018-12-05 17:21:19 +0000

    fixed random password genny

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,7 +1,15 @@
 class Employee < ActiveRecord::Base
+
   belongs_to :store
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, less_than: 200 }
   validates :store, presence: true
+  before_create :generate_password
+
+  private
+
+  def generate_password
+    self.password = (0...8).map { (65 + rand(26)).chr }.join
+  end
 end
--- lib/store.rb
+++ lib/store.rb
@@ -4,6 +4,14 @@ class Store < ActiveRecord::Base
   validates :name, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
 
+  def destroy
+    if self.employees.size > 0
+      return
+    else
+      super
+    end
+  end
+
   def must_carry_mens_or_womens_apparel
     if mens_apparel == false && womens_apparel == false
       errors.add(:mens_apparel, "ye got no clothes ye bassclawt")
